6fa5ef53654ed398355fa1748216b7e8bb0fd6a9
Jan-David Wiederstein <jan-david.wiederstein@codesphere.com>
2024-12-04 17:36:59 +0100

    implement pkl export for test data

--- Analyse_Training/utils/common/train_model.py
+++ Analyse_Training/utils/common/train_model.py
@@ -5,27 +5,27 @@ from sklearn.linear_model import LinearRegression
 from utils.common.print_evaluation import print_evaluation
 
 
-def train_model(merged_df: pd.DataFrame, columns: list, y_column: str) -> tuple:
+def train_model(test_df: pd.DataFrame, columns: list, y_column: str) -> tuple:
     """
     columns: list of columns to use for training
     y_column: column to predict
     """
     # one hot encode
-    merged_df = pd.get_dummies(merged_df)
+    test_df = pd.get_dummies(test_df)
 
     # get target column and store it in y
-    y = merged_df.pop(y_column)
+    y = test_df.pop(y_column)
     
     # split data into X and y
-    X_train, X_test, y_train, y_test = train_test_split(merged_df[columns], 
+    X_train, X_test, y_train, y_test = train_test_split(test_df[columns], 
                                                         y, 
                                                         test_size=0.2)
 
 
     # scale features
-    # scaler = StandardScaler()
-    # X_train = scaler.fit_transform(X_train)
-    # X_test = scaler.transform(X_test)
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
 
     # fit model
     model = LinearRegression()
@@ -46,6 +46,20 @@ def train_model(merged_df: pd.DataFrame, columns: list, y_column: str) -> tuple:
                                   0, 
                                   columns)
     
-    return model, evaluation
+    return model, evaluation, scaler, test_df[columns].columns
     
+
+def test_model(model, scaler, test_df, test_columns):
+    # one hot encode
+    test_df = pd.get_dummies(test_df)
+    
+    # scale features
+    X = scaler.transform(test_df[test_columns])
+    
+    # make predictions
+    y_pred = model.predict(X)
+
+    test_df['Solar_MWh_pred'] = y_pred
+    
+    return test_df
     
